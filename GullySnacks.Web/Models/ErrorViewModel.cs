60c9b66f46040ed3982de0a3f2469f891361f345
Ian Knighton <ian@knighton.dev>
2020-07-31 14:40:37 -0600

    Empty MVC

---
+++ GullySnacks.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GullySnacks.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
